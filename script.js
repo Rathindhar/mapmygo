ec49e6a647c3608275770f225fa9f3313b6df863
Rathindhar <rathindhar2004@gmail.com>
2026-01-05 23:58:00 +0530

    New update dummy

--- script.js
+++ script.js
@@ -112,5 +112,5 @@ function stopTracking() {
   journeys.push(journey);
   localStorage.setItem("journeys", JSON.stringify(journeys));
 
-  alert("Journey saved!");
+  alert("Journey saved! and updated");
 }
