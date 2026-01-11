38b6af3d1da81e14cf878650dd2fdea0dba172c5
Rathindhar <rathindhar2004@gmail.com>
2026-01-11 23:16:39 +0530

    he

--- script.js
+++ script.js
@@ -112,5 +112,5 @@ function stopTracking() {
   journeys.push(journey);
   localStorage.setItem("journeys", JSON.stringify(journeys));
 
-  alert("Journey saved Buddy and enjoy!");
+  alert("Journey saved Buddy and enjoy and have fun!");
 }
