af1734808e6c78e03989980d8463e71b70af5710
Rathindhar <rathindhar2004@gmail.com>
2026-02-01 23:48:04 +0530

    heii

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ function deleteRoute(index) {
   let journeys = JSON.parse(localStorage.getItem("journeys")) || [];
   if (!journeys[index]) return;
 
-  if (!confirm("Delete this route are iu sure?")) return;
+  if (!confirm("Delete this route are you sure??")) return;
 
   journeys.splice(index, 1);
   localStorage.setItem("journeys", JSON.stringify(journeys));
